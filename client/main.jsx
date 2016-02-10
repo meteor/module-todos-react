ff5da35d0e357d8250ceffd0e5b9d9ef12df7115
Evan You <yyx990803@gmail.com>
2016-02-10 10:24:10 -0500

    join page

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { AppContainer } from '../imports/containers/App.jsx';
 import { ListContainer } from '../imports/containers/List.jsx';
+import Join from '../imports/components/Join.jsx';
 import NotFound from '../imports/components/NotFound.jsx';
 
 Meteor.startup(() => {
@@ -10,6 +11,7 @@ Meteor.startup(() => {
     <Router history={browserHistory}>
       <Route path="/" component={AppContainer}>
         <Route path="lists/:id" component={ListContainer}/>
+        <Route path="join" component={Join}/>
         <Route path="*" component={NotFound}/>
       </Route>
     </Router>
---
+++ imports/components/Join.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import MobileMenu from './MobileMenu.jsx';
+import { Link } from 'react-router';
+import { Accounts } from 'meteor/accounts-password';
+
+export default class Join extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { errors: {}};
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+    const confirm = this.refs.confirm.value;
+    const errors = {};
+
+    if (!email) {
+      errors.email = 'Email required';
+    }
+    if (! password) {
+      errors.password = 'Password required';
+    }
+    if (confirm !== password) {
+      errors.confirm = 'Please confirm your password';
+    }
+
+    this.setState({ errors });
+    if (Object.keys(errors).length) {
+      return;
+    }
+
+    Accounts.createUser({
+      email,
+      password
+    }, err => {
+      if (err) {
+        this.setState({
+          errors: { 'none': error.reason }
+        });
+      }
+      this.context.router.push('/');
+    });
+  }
+
+  render() {
+    const { errors } = this.state;
+    const errorMessages = Object.keys(errors).map(key => errors[key]);
+    const errorClass = key => errors[key] && 'error';
+
+    return (
+      <div className="page auth">
+        <nav>
+          <MobileMenu/>
+        </nav>
+        <div className="content-scrollable">
+          <div className="wrapper-auth">
+            <h1 className="title-auth">Join.</h1>
+            <p className="subtitle-auth" >Joining allows you to make private lists</p>
+            <form onSubmit={this.onSubmit.bind(this)}>
+              {errorMessages.length
+                ? <div className="list-errors">
+                    {errorMessages.map(msg => (
+                      <div className="list-item" key={msg}>{msg}</div>
+                    ))}
+                  </div>
+                : null}
+              <div className={`input-symbol ${errorClass('email')}`}>
+                <input type="email" name="email" ref="email" placeholder="Your Email"/>
+                <span className="icon-email" title="Your Email"></span>
+              </div>
+              <div className={`input-symbol ${errorClass('password')}`}>
+                <input type="password" name="password" ref="password" placeholder="Password"/>
+                <span className="icon-lock" title="Password"></span>
+              </div>
+              <div className={`input-symbol ${errorClass('confirm')}`}>
+                <input type="password" name="confirm" ref="confirm" placeholder="Confirm Password"/>
+                <span className="icon-lock" title="Confirm Password"></span>
+              </div>
+              <button type="submit" className="btn-primary">Join Now</button>
+            </form>
+          </div>
+          <Link to="/signin" className="link-auth-alt">Have an account? Sign in</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+Join.contextTypes = {
+  router: React.PropTypes.object
+};
