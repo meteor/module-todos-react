0fde0a809a4b0e56df43e41150abaf75ea1f8703
Evan You <yyx990803@gmail.com>
2016-02-11 14:45:23 -0500

    rename client main file

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
-import { renderRoutes } from '../imports/startup/client/routes.jsx'
+import { renderRoutes } from '../imports/startup/client/routes.jsx';
 
 Meteor.startup(() => {
   render(renderRoutes(), document.getElementById('app'));
